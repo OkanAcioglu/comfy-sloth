65717319f4237b883d6ab9df9ee41e7be1d1d4c8
Okan Acıoğlu <okanacioglu@gmail.com>
2023-01-06 15:59:04 +0300

    styled-components-basics

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: green;
+  color: white;
+`
+
+const Container = styled.div`
+  background: red;
+  color: white;
+`
+
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <div>
+      <h4>comfy sloth starter</h4>
+      <Button>click me</Button>
+      <Container>
+        <p>Hello World</p>
+      </Container>
+    </div>
+  )
 }
 
 export default App
