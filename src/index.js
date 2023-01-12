11f0a1c1a219a2acab3c14902b9c44a85285ebc4
Okan <okanacioglu@gmail.com>
2023-01-12 23:05:22 +0300

    auth0-login-logout-toggle

--- src/index.js
+++ src/index.js
@@ -12,11 +12,21 @@ import { Auth0Provider } from '@auth0/auth0-react'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain={`${process.env.REACT_APP_DOMAIN}`}
+    clientId={`${process.env.REACT_APP_CLIENT_ID}`}
+    redirectUri={window.location.origin}
+    // cache location by default is not local storage... Below will make it local storage
+    cacheLocation='localstorage'
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 )
