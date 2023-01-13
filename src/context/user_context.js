01ad3a524404219985385b5418d610607a096b8b
Okan <okanacioglu@gmail.com>
2023-01-14 00:00:21 +0300

    AuthWrapper

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -3,8 +3,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 const UserContext = React.createContext()
 export const UserProvider = ({ children }) => {
-  const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
-    useAuth0()
+  const { loginWithRedirect, logout, user } = useAuth0()
 
   const [myUser, setMyUser] = useState(null)
 
@@ -12,12 +11,8 @@ export const UserProvider = ({ children }) => {
     //console.log(`user : ${user}`)
     //console.log(`isAuthenticated : ${isAuthenticated}`)
     //console.log(`isLoading : ${isLoading}`)
-    if (isAuthenticated) {
-      setMyUser(user)
-    } else {
-      setMyUser(false)
-    }
-  }, [isAuthenticated])
+    setMyUser(user)
+  }, [user])
 
   return (
     <UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
